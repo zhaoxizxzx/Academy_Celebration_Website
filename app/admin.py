3cc3965a7a3defa172501223e38a5c05c8c88718
Sirius-CYX <15059590067@163.com>
2022-11-28 00:26:26 +0800

    修缮部分BUG

--- app/admin.py
+++ app/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from app import models
 
+admin.site.site_title = "系统后台"
+
+admin.site.site_header = "管理员登录"
+
+admin.site.index_title = "后台管理"
 #admin注册表
 admin.site.register(models.UserInfo)
 admin.site.register(models.Blog)
