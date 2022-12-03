0504d3929dd5c1dff29acccda52ca6a1fdafbe3d
Sirius-CYX <15059590067@163.com>
2022-12-03 16:21:05 +0800

    完善后台管理功能及样式

--- app/admin.py
+++ app/admin.py
@@ -9,11 +9,15 @@ admin.site.site_header = "后台管理"
 admin.site.index_title = "后台管理"
 #admin注册表
 admin.site.register(models.UserInfo,UserAdmin)
+class UserInfoAdmin(admin.ModelAdmin):
+    list_filter = ('title',)
 admin.site.register(models.Blog)
 admin.site.register(models.Category)
-admin.site.register(models.Tag)
-admin.site.register(models.Article)
-admin.site.register(models.Article2Tag)
+class ArticleAdmin(admin.ModelAdmin):
+    list_filter = ('title',)
+admin.site.register(models.Article,ArticleAdmin)
 admin.site.register(models.UpAndDown)
 admin.site.register(models.Comment)
-admin.site.register(models.ClassesRecode)
+class ClassesRecodeAdmin(admin.ModelAdmin):
+    list_filter = ('class_name',)
+admin.site.register(models.ClassesRecode,ClassesRecodeAdmin)
